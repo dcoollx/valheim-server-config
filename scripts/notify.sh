77b372451f040f59a0a3465c69c4050e6f93a093
David Queen <dsqjr@msn.com>
2024-06-30 12:15:15 -0400

    scripts work in iso

--- scripts/notify.sh
+++ scripts/notify.sh
@@ -1,6 +1,4 @@
 #! /bin/bash
-if [[ $NOTIFY = "true" ]]; then
-    'curl -sfSL -X POST -H "Content-Type: application/json" -d "{\"username\":\"Valheim\",\"content\":\"$$(eval echo $$DISCORD_MESSAGE)\"}" "$$DISCORD_WEBHOOK"'
+if [ "$NOTIFY" = "true" ]; then
+    curl -sfSL -X POST -H "Content-Type: application/json" -d "{'username':'Valheim','content':'$DISCORD_MESSAGE'}" https://discord.com/api/webhooks/1254624822382821480/2lFskNf8dtSSFcO9gxZg6de07SxLnuwn2vHVhjEJMV233pqN5PSqIXAVZXQaoTMO3xQ-
 fi
-
-exit 0;
